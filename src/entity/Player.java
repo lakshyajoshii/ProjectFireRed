1a43ed97c9b1ef4cccf0c47d7d491ce758c6ea33
Lakshya Joshi <joshilakshya05@gmail.com>
2025-12-21 12:14:10 +0530

    Initial commit

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -23,6 +24,12 @@ public class Player extends Entity {
 		
 		screenX=gp.screenWidth/2 - (gp.tileSize/2);
 		screenY=gp.screenHeight/2 - (gp.tileSize/2);
+		
+		solidArea = new Rectangle();
+		solidArea.x=8;
+		solidArea.y=16;
+		solidArea.height=32;
+		solidArea.width=32;
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -54,16 +61,26 @@ public class Player extends Entity {
 		if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
 			if (keyH.upPressed == true) {
 				direction = "up";
-				worldY -= speed;
 			} else if (keyH.downPressed == true) {
 				direction = "down";
-				worldY += speed;
 			} else if (keyH.leftPressed == true) {
 				direction = "left";
-				worldX -= speed;
 			} else if (keyH.rightPressed == true) {
 				direction = "right";
-				worldX += speed;
+			}
+			//CHECK TILE COLLISION
+			collisionOn=false;
+			gp.collisionChecker.checkTile(this);
+			
+			//IF COLLISION IS FALSE, PLAYER CAN MOVE	
+			
+			if(collisionOn==false) {
+				switch(direction) {
+				case "up": worldY -= speed; break;
+				case "down": worldY += speed; break;
+				case "left": worldX -= speed; break;
+				case "right": worldX += speed; break;
+				}
 			}
 			spriteCounter++;
 			if (spriteCounter > 12) {
