f3113ea2e6ded1355ccc0bb69af925478b3fa009
Lakshya Joshi <joshilakshya05@gmail.com>
2026-01-19 00:48:36 +0530

    Objects Added

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
@@ -21,24 +22,24 @@ public class GamePanel extends JPanel implements Runnable {
 	public final int maxScreenRow = 12;
 	public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
 	public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
-	
-	//World Settings
-	
-	public final int maxWorldCol=50;
-	public final int maxWorldRow=50;
-	public final int WorldWidth=tileSize*maxWorldCol;
-	public final int WorldHeight=tileSize*maxWorldRow;
-	
-	//	FPS 
-	int fps=60;
-	
+
+	// World Settings
+
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+	public final int WorldWidth = tileSize * maxWorldCol;
+	public final int WorldHeight = tileSize * maxWorldRow;
+
+	// FPS
+	int fps = 60;
+
 	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
-	public CollisionChecker collisionChecker =  new CollisionChecker(this);
+	public CollisionChecker collisionChecker = new CollisionChecker(this);
+	public AssetSetter aSetter = new AssetSetter(this);
 	public Player player = new Player(this, keyH);
-	
-	
+	public SuperObject obj[] = new SuperObject[10];
 
 	public GamePanel() {
 
@@ -48,7 +49,10 @@ public class GamePanel extends JPanel implements Runnable {
 		this.addKeyListener(keyH);
 		this.setFocusable(true);
 	}
-
+	
+	public void setupGame() {
+		aSetter.setObject();
+	}
 	public void startGameThread() {
 		gameThread = new Thread(this);
 		gameThread.start();
@@ -76,44 +80,45 @@ public class GamePanel extends JPanel implements Runnable {
 //				e.printStackTrace();
 //			}
 //		}
-		//Delta Method
-		double drawInterval = 1000000000/fps;
-		double delta=0;
-		long lastTime=System.nanoTime();
+		// Delta Method
+		double drawInterval = 1000000000 / fps;
+		double delta = 0;
+		long lastTime = System.nanoTime();
 		long currentTime;
-		long timer=0;
-		int drawCount=0;
-		while(gameThread!=null) {
+		long timer = 0;
+		int drawCount = 0;
+		while (gameThread != null) {
 			currentTime = System.nanoTime();
-			delta+=(currentTime-lastTime)/drawInterval;
-			timer+=(currentTime - lastTime);
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
 			lastTime = currentTime;
-			
-			if(delta>=1) {
+
+			if (delta >= 1) {
 				update();
 				repaint();
 				delta--;
 				drawCount++;
 			}
-			if(timer>=1000000000) {
-				System.out.println("FPS : "+drawCount);
-				drawCount=0;
-				timer=0;
+			if (timer >= 1000000000) {
+				System.out.println("FPS : " + drawCount);
+				drawCount = 0;
+				timer = 0;
 			}
 		}
 	}
-	
+
 	public void update() {
 		player.update();
 	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		Graphics2D g2 = (Graphics2D)g;
+
+		Graphics2D g2 = (Graphics2D) g;
 		tileM.draw(g2);
-		
+
 		player.draw(g2);
-	
+
 		g2.dispose();
 	}
 
